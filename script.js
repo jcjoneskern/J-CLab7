1c5c07a7b125665bd5c969e8e03efe56c1cc72e8
J-C <jcjoneskern@gmail.com>
2016-11-03 09:32:12 -0400

    simplifications/changes the calculation to not make it a function

--- script.js
+++ script.js
@@ -17,12 +17,19 @@ var groceryList = [
 	}
 ];
 
-groceryList.makeList = function() {
-	var total = 0;
-	for(var i = 0; i < groceryList.length; i++) {
-		console.log(groceryList[i].name, "$"+groceryList[i].price.toFixed(2));
-		total += groceryList[i].price;
-	}
-	return total;
-};
-console.log("Total: $"+groceryList.makeList().toFixed(2));
+// groceryList.makeList = function() {
+// 	var total = 0;
+// 	for(var i = 0; i < groceryList.length; i++) {
+// 		console.log(groceryList[i].name, "$"+groceryList[i].price.toFixed(2));
+// 		total += groceryList[i].price;
+// 	}
+// 	return total;
+// };
+// console.log("Total: $"+groceryList.makeList().toFixed(2));
+
+var total = 0;
+for(var i = 0; i < groceryList.length; i++) {
+	console.log(groceryList[i].name, "$"+groceryList[i].price.toFixed(2));
+	total += groceryList[i].price;
+}
+console.log("Total: $"+total.toFixed(2));
